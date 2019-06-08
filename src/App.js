2317f5abffb6b9402157e7262f7fd0cacf69095f
Pantelis <tampantelis@gmail.com>
2019-06-08 21:40:36 +0300

    Everything done up to basic functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,87 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from './components/Header';
+import Weather from './components/Weather';
 import './App.css';
+import { css } from '@emotion/core';
+import { HashLoader } from 'react-spinners';
 
-function App() {
-  return (
-    <div className="App">
-      <Header  subtitle='Type a location to tell you its weather at the moment!'/>
+const App = () => {
+
+  const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
+
+  const [summary, setSummary ] = useState("");
+  const [location, setLocation ] = useState("");
+  const [temperature, setTemperature] = useState("");
+  const [precip, setPrecip] = useState("");
+  const [image, setImage] = useState("");
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState ('thessaloniki');
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  useEffect(()=> {
+   getWeather();
+  }, [query]);
+
+
+  const getWeather = async () => {
+    setIsLoading(true);
+    const responseLocation = await fetch(`https://cors-anywhere.herokuapp.com/https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=pk.eyJ1IjoiZGFudGVnciIsImEiOiJjanZhd2F1ZWowb3lkM3lxbWNwbXBmdW9lIn0.3gVF-ekrnv4XrVRJ4YBUmQ&limit=1`);
+    const dataLocation = await responseLocation.json();
+    setLocation(dataLocation.features[0].place_name);
+    const responseWeather = await fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/131c59f4dd9c0c2c3f1bca28f69c250d/${dataLocation.features[0].center[1]},${dataLocation.features[0].center[0]}?units=si`);
+    const dataWeather = await responseWeather.json();
+    setSummary(dataWeather.daily.summary);
+    setTemperature(dataWeather.currently.temperature);
+    setPrecip(dataWeather.currently.precipProbability);
+    setImage(dataWeather.currently.icon);
+    console.log(dataWeather);
+ 
+    setIsLoading(false);
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    console.log(search);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+
+    return (
+      <div className="App">
+        <div className="container">
+          <Header subtitle='Type a location to tell you its weather at the moment!'/>
+          <form onSubmit={getSearch} className="search-form">
+            <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+            <button className="search-button" type="submit">Search</button>
+          </form>
+          {isLoading ? (
+            <div className='sweet-loading'>
+            <HashLoader
+              css={override}
+              sizeUnit={"px"}
+              size={150}
+              color={'#006400'}
+            />
+          </div> 
+          ) : (
+            <div className="weather">
+                <Weather
+                place={location} summary={summary} temperature={temperature} image={image} precip={precip}/>
+     
+              </div>
+            )}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Weather.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import style from '../weather.module.css'
+
+const Weather = ({place, summary,temperature, image, precip}) => {
+
+  return(
+    <div className={style.weather}>
+      <h1>{place}</h1>
+      <img className={style.image} src={`https://darksky.net/images/weather-icons/${image}.png`} alt=""/>
+      <div className={style.content}>
+      <p>{summary} It is currently {temperature}C degrees out. There is {precip}% chance of rain today!</p>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Weather;
